7dbed71da75d023017fc6ebaf15d8e1d0e18f97b
victor <victor.lissoviy.93@gmail.com>
2023-08-14 13:47:52 +0300

    done HW3.2

--- HW3/src/main/java/main/Main.java
+++ HW3/src/main/java/main/Main.java
@@ -1,6 +1,52 @@
 package main;
 
+import java.util.AbstractMap;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class Main {
+
+  private static AbstractMap<String, String> createListFromUser() {
+    int n;
+    TreeMap<String, String> map = new TreeMap<>();
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter n:");
+    String nstr = scanner.nextLine();
+
+    try {
+      n = Integer.parseInt(nstr);
+    } catch (NumberFormatException ignored) {
+      n = 3;
+      System.out.println("n = 3");
+    }
+
+    for (int i = 0; i < n; i++) {
+      System.out.print("Enter city:");
+      String city = scanner.nextLine();
+
+      System.out.print("Enter last name:");
+      String lastName = scanner.nextLine();
+
+      map.put(city, lastName);
+    }
+
+    return map;
+  }
+
+  private static void findFamilyByCity(AbstractMap<String, String> map) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter city:");
+    String city = scanner.nextLine();
+
+    String lastName = map.get(city);
+
+    System.out.println(Objects.requireNonNullElse(lastName, "City Invalid :)"));
+  }
+
   public static void main(String[] args) {
+    AbstractMap<String, String> map = createListFromUser();
+    findFamilyByCity(map);
   }
 }
